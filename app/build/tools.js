4857eef46d61029a281a4830d0e385995381f89e
denzow <denzow@gmail.com>
2018-07-21 15:28:51 +0900

    [modify] tool.js reformat

--- app/build/tools.js
+++ app/build/tools.js
@@ -1,58 +1,58 @@
-const { resolve } = require('path')
-const { extract } = require('extract-text-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { resolve } = require('path');
+const { extract } = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 exports.htmlPage = (title, filename, chunks, template) => new HtmlWebpackPlugin({
   title,
   hash: true,
   cache: true,
   inject: 'body',
-  filename: './pages/' + filename + '.html',
+  filename: `./pages/${filename}.html`,
   template: template || resolve(__dirname, './page.ejs'),
   appMountId: 'app',
-  chunks
-})
+  chunks,
+});
 
 exports.cssLoaders = (options = {}) => {
-  let loaders = {}
-  let prePprocessors = {
+  const loaders = {};
+  const prePprocessors = {
     css: {},
     postcss: {},
     less: { loader: 'less' },
     sass: { loader: 'sass', options: { indentedSyntax: true } },
     scss: { loader: 'sass' },
     stylus: { loader: 'stylus' },
-    styl: { loader: 'stylus' }
-  }
-  for (let key in prePprocessors) {
-    let loader = [{
+    styl: { loader: 'stylus' },
+  };
+  for (const key in prePprocessors) {
+    const loader = [{
       loader: 'css-loader',
-      options: { minimize: process.env.NODE_ENV === 'production' }
-    }]
+      options: { minimize: process.env.NODE_ENV === 'production' },
+    }];
     if (prePprocessors[key].loader) {
       loader.push({
-        loader: prePprocessors[key].loader + '-loader',
-        options: Object.assign({}, prePprocessors[key].options, { sourceMap: options.sourceMap })
-      })
+        loader: `${prePprocessors[key].loader}-loader`,
+        options: Object.assign({}, prePprocessors[key].options, { sourceMap: options.sourceMap }),
+      });
     }
     if (options.extract) {
-      loaders[key] = extract({ use: loader, fallback: 'vue-style-loader' })
+      loaders[key] = extract({ use: loader, fallback: 'vue-style-loader' });
     } else {
-      loaders[key] = ['vue-style-loader'].concat(loader)
+      loaders[key] = ['vue-style-loader'].concat(loader);
     }
   }
-  return loaders
-}
+  return loaders;
+};
 
-exports.styleLoaders = function (options) {
-  const output = []
-  const loaders = exports.cssLoaders(options)
+exports.styleLoaders = (options) => {
+  const output = [];
+  const loaders = exports.cssLoaders(options);
   for (const extension in loaders) {
-    const loader = loaders[extension]
+    const loader = loaders[extension];
     output.push({
-      test: new RegExp('\\.' + extension + '$'),
-      use: loader
-    })
+      test: new RegExp(`\\.${extension}$`),
+      use: loader,
+    });
   }
-  return output
-}
+  return output;
+};
